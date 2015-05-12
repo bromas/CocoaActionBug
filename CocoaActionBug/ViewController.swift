f5c0aa5a8dd861585b9925af66c880450e16e87e
Siemen Sikkema <siemen@unclutter.nl>
2015-05-12 16:01:49 +0200

    Initial Commit

---
+++ CocoaActionBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaActionBug
+//
+//  Created by Siemen Sikkema on 12/05/15.
+//  Copyright (c) 2015 Unclutter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
